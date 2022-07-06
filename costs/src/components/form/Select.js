773eebfd21d1a5bb3e6e74c0da979e06474e8fec
Michael <michaelpetterson06.com.br>
2022-07-06 12:43:01 -0300

    executando um fetch na API categories no component ProjectForm

--- costs/src/components/form/Select.js
+++ costs/src/components/form/Select.js
@@ -1,11 +1,21 @@
 import Styles from './Select.module.css';
 
-function Select ({text, name, option, handleOnChange, value}) {
+function Select ({text, name, options, handleOnChange, value}) {
+   
     return (
         <div className={ Styles.form_control }>
             <label htmlFor={ name }>{ text }:</label>
             <select name={ name } id={ name }>
                 <option>Selecione uma opção</option>
+                { options.map((option) => (
+                    <option
+                        value={ option.id}
+                        key={ option.id }
+                    >
+                        { option.name}
+                            
+                    </option>
+                ))}
             </select>
         </div>
     )
--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -1,9 +1,33 @@
+import React, { useState, useEffect } from 'react';
 import Styles from './ProjectForm.module.css'
 import Input from '../form/input.js'
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 
 function ProjectForm  ({btnText}) {
+
+    const [categories, setCategories ] = useState([]);
+
+    useEffect(() => {
+        fetchCategories();
+    },[])
+
+    
+    const fetchCategories = () => {
+        fetch('http://localhost:5001/categories', {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data)
+      })
+      .catch((err) => console.log(err))
+    }
+    
+
+
     return (
         <form className={ Styles.form }>
             <Input
@@ -18,7 +42,11 @@ function ProjectForm  ({btnText}) {
                 name='budget'
                 placeholder='Insira o orçamento total'
             />
-            <Select name='category_id' text='Selecione uma categoria'/>
+            <Select
+                name='category_id'
+                text='Selecione uma categoria'
+                options={categories}
+            />
             <SubmitButton text={ btnText }/>
         </form>
     )
