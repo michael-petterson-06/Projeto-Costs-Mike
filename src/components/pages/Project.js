6df23c71bd3788eb7c2c6139829732a112e75492
Michael <michaelpetterson06.com.br>
2022-07-07 17:45:59 -0300

    estilizando e aplicando funcionalidades no component project

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,6 +1,8 @@
-import { useParams } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-// import styles from './Project.module.css'
+import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import Loading from '../layout/Loading'
+import Container from '../layout/Container';
+import styles from './Project.module.css';
 
 
 function Project () {
@@ -8,14 +10,16 @@ function Project () {
     let { id } = useParams()
    
     const [project, setProject ] = useState([]);
+    const [ showProjectForm, setShowProject ] = useState(false);
 
     useEffect(() => {
-        getProject();
+        setTimeout(() => {
+            getProject();
+        }, 1000)
     }, [])
 
      
     const getProject = () => {
-            
         fetch(`http://localhost:5001/projects/${id}`, {
         method: 'GET',
         headers: {
@@ -27,8 +31,44 @@ function Project () {
       })   
       .catch((error) => console.log(error))
    }
+
+   function toggleProjectForm () {
+        setShowProject(!showProjectForm);
+   }
   
-    return <div>{project.name}</div>
-}    
+    return (
+        <React.Fragment>
+            { project.name ? (
+                <div className={ styles.project_details }>
+                    <Container customClass='column'>
+                        <div className={ styles.details_container }>
+                            <h1>Projeto: { project.name }</h1>
+                            <button onClick={ toggleProjectForm } className={ styles.btn }>
+                                { !showProjectForm ? 'Editar projeto' : 'Fechar'}
+                            </button>
+                            { !showProjectForm ? (
+                                <div className= { styles.project_info}>
+                                    <p>
+                                        <span>Categoria: </span>{ project.category.name}
+                                    </p>
+                                    <p>
+                                        <span>Total de Orçamento: </span> R${project.budget}
+                                    </p>
+                                    <p>
+                                        <span>Total de Utilizado: </span> R${project.cost}
+                                    </p>
+                                </div>
+                            ) : (
+                                <div className= { styles.project_info}>
+                                    <p>Form</p>
+                                </div>
+                            )}
+                        </div>
+                    </Container>
+                </div> ) : (
+                <Loading/>) }
+        </React.Fragment>
+    )
+}     
 
 export default Project;
