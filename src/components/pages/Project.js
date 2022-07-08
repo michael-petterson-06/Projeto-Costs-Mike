16927407d084233830ac2ab8a6c6685746d8b2d9
Michael <michaelpetterson06.com.br>
2022-07-08 19:16:59 -0300

    testando commits nova branch

---
+++ src/components/pages/Project.js
@@ -0,0 +1,107 @@
+import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import Loading from '../layout/Loading'
+import Container from '../layout/Container';
+import styles from './Project.module.css';
+import ProjectForm from '../project/ProjectForm';
+import Message from '../layout/Message';
+
+
+function Project () {
+
+    let { id } = useParams()
+   
+    const [project, setProject ] = useState([]);
+    const [ showProjectForm, setShowProject ] = useState(false);
+    const [ message, setMessage ] = useState('');
+    const [ type, setType ] = useState('');
+
+    useEffect(() => {
+        setTimeout(() => {
+            getProject();
+        }, 1000)
+    }, [])
+
+     
+    const getProject = () => {
+        fetch(`http://localhost:5001/projects/${id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }).then((resp) => resp.json())
+      .then((data) => {
+        setProject(data)
+      })   
+      .catch((error) => console.log(error))
+   }
+
+   function toggleProjectForm () {
+        setShowProject(!showProjectForm);
+   }
+
+   function editPost (project) {
+        setMessage('')  
+        if (project.budget < project.cost) {
+            setMessage('Não há verba sufuciente para esse serviço')
+            setType('error')
+            return false
+        }
+        fetch(`http://localhost:5001/projects/${project.id}`,{
+            method: 'PATCH', // só alterar oque for enviado
+            headers: {
+                'content-Type': 'application/json', // para json se comunicar com API
+            },
+            body: JSON.stringify(project), //mando o projeto com texto para atualizar
+        }).then(resp => resp.json() )
+          .then((data) => {
+            setProject(data)
+            setShowProject(!showProjectForm);
+            setMessage('Projeto atualizado com sucesso!!!')
+            setType('success')
+          })   
+          .catch((error) => console.log(error))
+   }
+  
+   
+    return (
+        <React.Fragment>
+            { project.name ? (
+                <div className={ styles.project_details }>
+                    <Container customClass='column'>
+                        { message && <Message type={ type } msg={ message }/>}
+                        <div className={ styles.details_container }>
+                            <h1>Projeto: { project.name }</h1>
+                            <button onClick={ toggleProjectForm } className={ styles.btn }>
+                                { !showProjectForm ? 'Editar projeto' : 'Fechar'}
+                            </button>
+                            { !showProjectForm ? (
+                                <div className= { styles.project_info}>
+                                    <p>
+                                        <span>Categoria: </span>{ project.category.name}
+                                    </p>
+                                    <p>
+                                        <span>Total de Orçamento: </span> R${project.budget}
+                                    </p>
+                                    <p>
+                                        <span>Total de Utilizado: </span> R${project.cost}
+                                    </p>
+                                </div>
+                            ) : (
+                                <div className= { styles.project_info}>
+                                    <ProjectForm
+                                        handleSubmit={ editPost }
+                                        btnText='Concluir edição'
+                                        projectData={ project }
+                                    />
+                                </div>
+                            )}
+                        </div>
+                    </Container>
+                </div> ) : (
+                <Loading/>) }
+        </React.Fragment>
+    )
+}     
+
+export default Project;
---
+++ src/components/pages/Projects.js
@@ -0,0 +1,91 @@
+import { useState, useEffect } from 'react'; 
+import { useLocation } from 'react-router-dom';
+import Message from '../layout/Message.js';
+import styles from './Projects.module.css';
+import Container from '../layout/Container.js'
+import LinkButton from '../layout/LinkButton.js';
+import ProjectCard from '../project/ProjectCard.js';
+import Loading from '../layout/Loading'
+
+function Projects() {
+
+    const [ projects, setProjects ] = useState([])
+    const [ removeLoading, setRemoveLoading] = useState(false)
+    const [ projectMessage, setProjectMessage ] = useState('')
+    
+    const location = useLocation()
+    
+    let message = '';
+    
+    if (location.state){
+        message = location.state.message
+    }
+
+   
+    useEffect(() => {
+        setTimeout(() => {
+            getProjects()
+        }, 1000)
+    }, [])
+
+   
+    const getProjects =()=>{
+        fetch('http://localhost:5001/projects',{
+            method:'GET',
+            header:{
+                'content-Type':'application/json',
+            },
+        }).then((resp) => resp.json())
+          .then((data) => {
+            setProjects(data)
+            setRemoveLoading(true)
+         })
+          .catch((error) => console.log(error))
+    }
+
+
+    function removeProject(id) {
+        fetch(`http://localhost:5001/projects/${id}`,{
+            method: 'DELETE',
+            headers: {
+               'Content-Type': 'application/json',
+            },
+        }).then(resp => resp.json())
+          .then(data => {
+                setProjects(projects.filter((project) => project.id !== id))
+                setProjectMessage('Projeto removido com sucesso!!!')
+          })  
+    }
+   
+    
+    return (
+        <div className={ styles.project_container }>
+            <div className={ styles.title_container }>
+                <h1>Projetos</h1>
+                <LinkButton to='/newproject' text='Criar Projeto'/>
+            </div>
+            { message && <Message msg={ message } type='success'/> }
+            {projectMessage && <Message msg={projectMessage} type='success'  />}
+            <Container  customClass='start'>
+                { projects.length > 0 &&
+                 projects.map((project) =>(
+                    <ProjectCard
+                        id={ project.id}
+                        name={ project.name }
+                        budget={ project.budget }
+                        category= { project.category.name}
+                        key={project.id}
+                        handleRemove={ removeProject }
+                    />
+                ))}
+                { !removeLoading && <Loading />}
+                { removeLoading && projects.length === 0 &&
+                    <p>Não há projetos cadastrados</p>
+                }
+            </Container>
+        </div>
+    ) 
+}
+
+
+export default Projects;
